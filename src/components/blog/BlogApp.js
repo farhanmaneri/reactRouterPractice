8b3eb2bf467255d03129feae6dc683200cef7d3d
farhanmaneri <farhanmaneri@gmail.com>
2023-07-10 21:10:12 -0700

    react routerreact scss and ant design practice

---
+++ src/components/blog/BlogApp.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function BlogApp() {
+  return (
+    <div>
+      blog
+    </div>
+  )
+}
+
+export default BlogApp;
---
+++ src/components/userApp/UserApp.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function UserApp() {
+  return (
+    <div>
+      user app page
+    </div>
+  )
+}
+
+export default UserApp;
