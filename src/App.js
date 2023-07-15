593f9df40b52c9e679509c38266b352a2b7106d5
farhanmaneri <farhanmaneri@gmail.com>
2023-07-15 09:05:31 -0700

    routing done

--- src/App.js
+++ src/App.js
@@ -4,8 +4,9 @@ import Routes from './routes/Routes';
 
 function App() {
   return (
+   
+  <Routes /> 
   
-  <Routes/>
       );
 }
 
--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -1,17 +1,22 @@
-import {  
-    Route,
-    Routes as AppRoutes,
+import {   Route,  Routes as AppRoutes,
   } from "react-router-dom";
 import Home from "../components/home/Home";
 import BlogApp from "../components/blog/BlogApp";
 import UserApp from "../components/userApp/UserApp";
 
+
 const Routes = ()=> {
-<AppRoutes>
+ 
+ return(
+
+   <AppRoutes>
+
 <Route path="/" element={<Home/>} />
-<Route path="/blog/*" element={<BlogApp />} />
-<Route path="/users/*" element={<UserApp />} />
+<Route path="/blog" element={<UserApp/>} />
+<Route path="/users" element={<BlogApp/>} />
 </AppRoutes>
+  )
+
 }
 export default Routes;
 
